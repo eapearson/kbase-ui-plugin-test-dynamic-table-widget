44cd5f2c5c0160753beec40baf8abd6e0342ab86
Erik Pearson <eapearson@lbl.gov>
2017-05-11 16:18:53 -0700

    initial check in of this new test plugin

---
+++ src/plugin/modules/panel.js
@@ -0,0 +1,145 @@
+define([
+    'jquery',
+    'bluebird',
+    'kb_common/html',
+    'kb_common/dynamicTable',
+
+    'bootstrap'
+], function(
+    $,
+    Promise,
+    html,
+    DynamicTable
+) {
+    'use strict';
+
+    var t = html.tag,
+        div = t('div'),
+        p = t('p');
+
+    function factory(config) {
+        var hostNode, container,
+            runtime = config.runtime,
+            testId = html.genId();
+
+        // RENDERING CODE, ETC.
+
+        function testIt() {
+            var dummyRows = [
+                    [1, 2, 3],
+                    [4, 5, 6]
+                ],
+                dummyHeaders = [{
+                    id: 'col1',
+                    text: 'Column 1',
+                    isSortable: true
+                }, {
+                    id: 'col2',
+                    text: 'Column 2',
+                    isSortable: true
+                }, {
+                    id: 'col3',
+                    text: 'Column 3',
+                    isSortable: false
+                }],
+                $dummyDiv = $('#' + testId);
+
+            new DynamicTable($dummyDiv, {
+                headers: dummyHeaders,
+                updateFunction: function(pageNum, query, sortColId, sortColDir) {
+                    return Promise.try(function() {
+                        return {
+                            start: 0,
+                            total: dummyRows.length,
+                            rows: dummyRows
+                        };
+                    });
+                }
+            });
+
+        }
+
+        // Note: this is not a special function, just a markup-building
+        // function, in this case simply syncronous.
+        function render() {
+            // Typically for the top level of a plugin you need to wrap in bootstrap
+            // boilerplate in order to get the padding, etc. that other top level
+            // panels do.
+            return div({
+                class: 'container-fluid'
+            }, [
+                div({
+                    class: 'row'
+                }, [
+                    div({
+                        class: 'col-md-6'
+                    }, 'This is my plugin.'),
+                    div({
+                        class: 'col-md-6'
+                    }, [
+                        div({
+                            id: testId
+                        })
+                    ])
+                ])
+            ]);
+        }
+
+        // LIFECYCLE API
+
+        function init(config) {
+            return Promise.try(function() {
+                // anything to initialize?
+            });
+        }
+
+        function attach(node) {
+            return Promise.try(function() {
+                // Make our own top level container node. 
+                // Sometimes we might want to create an html 
+                // layout here.
+                hostNode = node;
+                container = hostNode.appendChild(document.createElement('div'));
+            });
+        }
+
+        function start(params) {
+            return Promise.try(function() {
+                // usually rendering, or long async data loading, content generation, 
+                // would go here.
+                runtime.send('ui', 'setTitle', 'Dynamic Table Test')
+                container.innerHTML = render();
+                testIt();
+            });
+        }
+
+        function stop() {
+            return Promise.try(function() {
+                // anything to stop?
+            });
+        }
+
+        function detach() {
+            return Promise.try(function() {
+                // at a minimum be nice and remove our top level node if any.
+                if (hostNode && container) {
+                    hostNode.removeChild(container);
+                }
+            });
+        }
+
+        return {
+            init: init,
+            attach: attach,
+            start: start,
+            stop: stop,
+            detach: detach
+        };
+    }
+
+    return {
+        make: function(config) {
+            return factory(config);
+        }
+    };
+});
